11235722d67856d764aa22731d8150a33888f40f
Milky2018 <842376130@qq.com>
2026-05-07 16:48:44 +0800

    Line-buffer Mooncraft Docker logs

--- docker/mooncraft/entrypoint.sh
+++ docker/mooncraft/entrypoint.sh
@@ -7,4 +7,4 @@ if [[ $# -gt 0 ]]; then
   exec "$@"
 fi
 
-exec moon -C /app run --target native --release services/control-plane
+exec stdbuf -oL -eL moon -C /app run --target native --release services/control-plane
